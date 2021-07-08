4a18619c3bd29aba1caf42758de4a8a9c62d37c5
Adrian Ferenc <68762863+adrianferenc@users.noreply.github.com>
2021-07-08 11:29:45 -0700

    restructure directory

--- app/server.js
+++ app/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('./config/database')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -22,8 +23,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-require('./config/database')
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
