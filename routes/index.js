9c364ff84b229a011bf5004ebe2320ff465e2d9c
Adrian Ferenc <68762863+adrianferenc@users.noreply.github.com>
2021-07-14 16:24:07 -0700

    adds search functionality

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const Student = require("../models/student");
+const Narrative = require("../models/narrative");
+const Category = require("../models/category");
 
 /* GET home page. */
 router.get("/", function (req, res) {
@@ -12,6 +15,33 @@ router.get("/about", function (req, res) {
 
 router.get("/profile", function (req, res) {
   res.render("profile", { title: "Profile", navKey: "profile" });
+});
+
+router.get("/search", async function (req, res) {
+  const query = req.url.slice(15).toLowerCase().replace(/\++/gm, " ");
+  const foundStudents = await Student.find({
+    name: { $regex: query, $options: "i" },
+  });
+
+  const foundNarratives = await Narrative.find({
+    text: { $regex: query, $options: "i" },
+  })
+
+
+  const foundNarrativeNames = {};
+
+  for (narrative of foundNarratives){
+    let thisStudent = await Student.findById(narrative.student);
+    foundNarrativeNames[narrative.student] = thisStudent.name;
+  }
+
+  res.render("search.ejs", {
+    title: "Search Results",
+    navKey: "home",
+    foundStudents,
+    foundNarratives,
+    foundNarrativeNames
+  });
 });
 
 module.exports = router;
